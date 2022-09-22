bbab7d5906b84d34a5b3f9c546acaba44e71530c
woICcc <113985538+woICcc@users.noreply.github.com>
2022-09-22 15:01:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ export const config = {
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "eeb44860e7702db513672fc942f55658",
+  APP_SECRET: "c2e769638f2c9b097daee2e0a9a363a7",
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
